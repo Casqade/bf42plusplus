576e1b0f5b4a890a778c919fa93fb37760365fce
casqade <ivamspasibo@gmail.com>
2026-05-06 01:46:01 +0300

    fix: odr violation

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-const wchar_t* build_version = L"2.0";
-const char* build_version_full = "2.0.0-0-0";
+inline const wchar_t* build_version = L"2.0";
+inline const char* build_version_full = "2.0.0-0-0";
